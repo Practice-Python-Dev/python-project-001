3a5ec25913dc930171d272296ee1891be38eeb5b
James N <67208442+jamesnorbertowork@users.noreply.github.com>
2020-08-02 20:36:04 +0900

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,47 +1,55 @@
-# PLAY AROUND WITH THE ALPHABET
+#------------------------------
+# COUNT VOWELS IN THE ALPHABET
+#------------------------------
 
-# Initialize variable containing the alphabet (lower case), and start vowel count from 0
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# Initialize alphabet in a string, Initialize a counter
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 vowels = 0
-# Use a for loop to check if/or. Add to 'vowels' if True.
+
+# Check using 'if/or' - Add to 'vowels' if True
 for i in alphabet:
-    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
         vowels += 1
-print('There are', str(vowels), 'vowels in the alphabet.')
-print('\n' *1)
+print("There are", str(vowels), "vowels in the alphabet.")
+print("\n")
 
+#------------------------------
+# ASK FOR A LETTER IN THE ALPHABET
+#------------------------------
 
-
-
-# Ask for a letter in the alphabet, ensure to point to it's lowercase
-user_letter = input('Enter one letter')
+# Ask user for a letter, Ensure it's lowercase
+user_letter = input("Enter a Letter: ")
 user_letter = user_letter.lower()
 
-# Loop through the alphabet. If the letter is in then print it and 'break' out of the for loop. If it isn't print sorrr ...
+# If it's in the alphabet print and 'break' - If not print an 'error message'
 for i in range(len(alphabet)):
     if alphabet[i] == user_letter:
-        print('You entered', user_letter, 'which is in the alphabet!')
+        print("You entered", user_letter, "which is in the alphabet!")
         break
 else:
-    print('Sorry. You entered', user_letter, ', which isn\'t a letter ...')
-print('\n' *1)
+    print("Sorry. You entered", user_letter, "which isn\'t a letter ...")
+print("\n" *1)
 
+#------------------------------
+# SAME, BUT PRINT IN A COOL WAY
+#------------------------------
 
+# Initialize all possible letters, Initialize a counter
+all_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+counter = 0
 
-# This time initialize all possible letters. Use a 'while' loop to check and print in a cool way!
+#Ask user for input, Ask how many times to print
+user_word = input("Enter a word, so I can print it back to you:")
+how_many_times = int(input("How many times do you want to hear your word?"))
 
-# Initialze all letters. Ask user for word. Ask user how many times to print.
-all_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-user_word = input('Enter a word, so I can print it back to you:')
-how_many_times = int(input('How many times do you want to hear your word?'))
+# Use a 'while' loop to check and print
 
-i = 0
-while i < len(user_word):
-    char = user_word[i]
+while counter < len(user_word):
+    char = user_word[index]
     if char in all_letters:
-        print('Give me an', char, '!')
-    i += 1
-print('What does that spell?')
-for i in range(how_many_times):
-    print(user_word, '!!!')
-print('\n' *1)
+        print("Give me an", char, "!")
+    counter += 1
+print("What does that spell?")
+for counter in range(how_many_times):
+    print(user_word, "!!!")
+print("\n")
